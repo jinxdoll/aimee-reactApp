6d29627f88fcde06125b4a3aaf630159676005fa
Aimee <amattson84@gmail.com>
2021-04-07 16:13:57 -0700

    Setting up router

---
+++ second-build/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+// const mongoose = require("mongoose");
+const ApiRouter = require("./routes/api");
+
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/test");
+
+// mongoose.connection.on("error", (err) =>
+//   console.log("error in connection: ", err)
+// );
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/", ApiRouter);
+
+// need to render the website here where it says "website"
+app.use("*", ((req, res) => {
+    res.sendFile(path.join(__dirname,"./public/index.html" ))
+})
+);
+
+app.listen(PORT, (PORT, error) =>
+  error 
+  ? console.log(" error is:", error);
+  : `server is listening on ${PORT}`
+);
